4df8ceae7453d05ecc70c0197c3ce81f497bd77c
Alexander Conroy <alex@esotech.org>
2013-09-28 23:05:29 -0400

    Figuring out SPlits.

--- test.js
+++ test.js
@@ -29,24 +29,33 @@ var periodicTable = require('./periodictable.js');
 
 var util = require('util');
 
-process.stdin.on('data', function (text) {
-  var input = text.split();
-  var elements = [];
-  for (var symbol in input){
-    for (var e in periodicTable.table) {
-      if(periodicTable.table[e].symbol == input[symbol]){
-          elements.push(periodicTable.table[e]);
-          break;
-      }
-    }
-  }
-  console.log(elements);
-  if (text === 'quit') {
-    done();
-  }
+process.stdin.on('data', function(text) {
+	var input = text.replace(/(\r\n|\n|\r)/gm, "").split(/(?=[A-Z])/);
+	var coefficient = 1;
+	if (parseInt(input[0], 10) > 0) {
+		coefficient = parseInt(input[0], 10);
+		input.shift();
+	}
+	console.log(coefficient, input);
+	return;
+	var elements = [];
+	for (var symbol in input) {
+		loop2: for (var e in periodicTable.table) {
+			console.log(periodicTable.table[e]);
+			if (periodicTable.table[e].symbol == input[symbol]) {
+				elements.push(periodicTable.table[e]);
+				break loop2;
+			}
+		}
+	}
+	console.log(elements);
+	if (text === 'quit') {
+		done();
+	}
+	return;
 });
 
 function done() {
-  console.log('Now that process.stdin is paused, there is nothing more to do.');
-  process.exit();
+	console.log('Now that process.stdin is paused, there is nothing more to do.');
+	process.exit();
 }
